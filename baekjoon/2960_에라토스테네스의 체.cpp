d8a7cae0810cb2dd0376cc2493ed1e06b85026ad
Hwang In Seo <sjsjsj1246@gmail.com>
2019-01-11 20:41:31 +0900

    0111

---
+++ baekjoon/2960_에라토스테네스의 체.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+bool check[1001] = {1,1,}; //0일때 소수
+
+int main(void)
+{
+	int n, k, cnt=0;
+	cin >> n >> k;
+	for (int i = 2; i < n+1; i++) 
+	{
+		if (check[i] == 0)
+		{
+			cnt++;
+			if (cnt == k)
+			{
+				cout << i;
+				return 0;
+			}
+			for (int j = 2; i*j <n+1; j++)
+			{
+				if (check[i*j] == 0) cnt++;
+				check[i*j] = 1; 
+				if (cnt == k)
+				{
+					cout << i*j;
+					return 0;
+				}
+			}
+		}
+	}
+
+}
