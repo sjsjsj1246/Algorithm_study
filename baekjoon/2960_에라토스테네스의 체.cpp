0c290ee8b49a5c6b404de9f2a0cb863b9c919585
Hwang In Seo <sjsjsj1246@gmail.com>
2019-01-14 19:39:33 +0900

    Update 2960_에라토스테네스의 체.cpp

--- baekjoon/2960_에라토스테네스의 체.cpp
+++ baekjoon/2960_에라토스테네스의 체.cpp
@@ -1,34 +1,33 @@
 #include <iostream>
-
+#include <vector>
+#include <string>
+#include <cstring>
+#include <functional>
+#include <algorithm>
+#define MOD 1000000000
+#define fast ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 using namespace std;
 
-bool check[1001] = {1,1,}; //0일때 소수
+bool check[1001] = { 1,1, };
 
 int main(void)
 {
-	int n, k, cnt=0;
+	int n, k, cnt = 0;
 	cin >> n >> k;
-	for (int i = 2; i < n+1; i++) 
+	for (int i = 2; i < n + 1; i++)
 	{
 		if (check[i] == 0)
 		{
-			cnt++;
-			if (cnt == k)
-			{
-				cout << i;
-				return 0;
-			}
-			for (int j = 2; i*j <n+1; j++)
+			for (int j = 1; i*j <n + 1; j++)
 			{
 				if (check[i*j] == 0) cnt++;
-				check[i*j] = 1; 
+				check[i*j] = 1;
 				if (cnt == k)
 				{
-					cout << i*j;
+					cout << i * j;
 					return 0;
 				}
 			}
 		}
 	}
-
 }
