ce30f13be79857ff72ef8b2d6473a93fd7f87ccf
Hwang In Seo <sjsjsj1246@gmail.com>
2019-02-11 15:26:47 +0900

    0211 문제풀기

---
+++ baekjoon/6131_완전 제곱수.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <set>
+#include <stack>
+#include <queue>
+#include <map>
+#include <cmath>
+#include <string>
+#include <ctime>
+using namespace std;
+typedef signed long long LL;
+
+#define FOR(x,to) for(int x=0;x<(to);x++)
+#define ITR(x, arr) for(auto x = arr.begin(); x!= arr.end(); x++)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,-1,sizeof(a))
+#define FAST ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+//----------------------------------------------------------
+
+int main()
+{
+	int n, ans = 0;
+	cin >> n;
+	for (int a = 1; a <= 500; a++)
+	{
+		for (int b = 1; b < a; b++)
+		{
+			if (a*a - b * b == n) ans++;
+		}
+	}
+	cout << ans;
+}
