ae8dd4cf504e9dedf2411187a0260d8ca47c6c56
Hwang In Seo <sjsjsj1246@gmail.com>
2019-02-25 19:18:18 +0900

    0225 문제풀이

---
+++ baekjoon/16435_스네이크버드.cpp
@@ -0,0 +1,64 @@
+// need
+#include <iostream>
+#include <algorithm>
+// data structure
+#include <bitset>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+#include <deque>
+#include <array>
+// stream
+#include <istream>
+#include <sstream>
+#include <ostream>
+// etc
+#include <cstdlib>
+#include <cmath>
+#include <functional>
+#include <chrono>
+#include <random>
+#include <cstring>
+using namespace std;
+typedef long long int ll;
+// input
+#define ALL(a) (a).begin(),(a).end()
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,-1,sizeof(a))
+#define CASES(t) int t; cin >> t; while(t--)
+#define FAST ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define MOD 1000000007
+#define INF 987654321
+#define SUB(a) cout << a << "\n";
+// output
+#define SP << " "
+#define BR << "\n"
+#define SPBR(i, n) <<(i + 1 == n ? '\n' : ' ');
+#define SHOWVECTOR(v) {std::cerr << #v << "\t:";for(const auto& xxx : v){std::cerr << xxx << " ";}std::cerr << "\n";}
+#define SHOWVECTOR2(v) {std::cerr << #v << "\t:\n";for(const auto& xxx : v){for(const auto& yyy : xxx){std::cerr << yyy << " ";}std::cerr << "\n";}}
+#define SHOWQUEUE(a) {auto tmp(a);std::cerr << #a << "\t:";while(!tmp.empty()){std::cerr << tmp.front() << " ";tmp.pop();}std::cerr << "\n";}
+// utility
+#define FOR(w, a, n) for(int w=(a);w<(n);++w)
+#define RFOR(w, a, n) for(int w=(n)-1;w>=(a);--w)
+#define ITR(it, vec) for(auto it = vec.begin(); it!= vec.end(); it++)
+#define RITR(it, vec) for(auto it = vec.rbegin(); it!= vec.rend(); it++)
+template<typename S, typename T>
+std::ostream& operator<<(std::ostream& os, std::pair<S, T> p) {
+	os << "(" << p.first << ", " << p.second << ")"; return os;
+}
+//----------------------------------------------------------
+
+int main()
+{
+	FAST;
+	int n, l;
+	cin >> n >> l;
+	vector<int> arr(n);
+	FOR(i, 0, n) cin >> arr[i];
+	sort(ALL(arr));
+	for (auto x : arr) if (l >= x) l++;
+	cout << l;
+}
