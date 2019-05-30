5475a44ecff1b5657077ceb8b466bf2010d608ea
Inseo Hwang <sjsjsj1246@gmail.com>
2019-05-30 20:40:32 +0900

    Update 1079_안홍현의 더 이상 참을 수 없다.cpp

--- 안홍현.com/1079_안홍현의 더 이상 참을 수 없다.cpp
+++ 안홍현.com/1079_안홍현의 더 이상 참을 수 없다.cpp
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-int arr[11][11];
+int arr[101][101];
 
 int main()
 {
-	int n, m;
-	scanf("%d%d", &n, &m);
+	int n;
+	scanf("%d%d", &n);
 	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < m; j++)
+		for (int j = 0; j < n; j++)
 		{
 			scanf("%d", &arr[i][j]);
 		}
@@ -20,7 +20,7 @@ int main()
 	int ans = 0;
 	for (int i = 0; i < n - p + 1; i++)
 	{
-		for (int j = 0; j < m - p + 1; j++)
+		for (int j = 0; j < n - p + 1; j++)
 		{
 			int cnt = 0;
 			for (int x = i; x < i + p; x++)
