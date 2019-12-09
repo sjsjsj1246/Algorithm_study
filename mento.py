5a45b1f64991138fdb0fdb02b0f345e07d2f6a74
sjsjsj1246 <sjsjsj1246@gmail.com>
2019-12-09 23:31:22 +0900

    .

--- mento.py
+++
@@ -1,9 +0,0 @@
-import random
-print("재학생 리스트 입력")
-z = list(input().split(' '))
-print("신입생 리스트 입력")
-s = list(input().split(' '))
-random.shuffle(s)
-random.shuffle(z)
-for i in range(len(z)):
-    print("멘토 : " + z[i] + " 멘티 : "+ s[i])
